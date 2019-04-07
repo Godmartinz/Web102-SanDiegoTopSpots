ece230b497372dc078f46b056bbbfacec104b7f1
godmartinz <godmartinz@gmail.com>
2019-04-07 14:13:57 -0700

    adds initial project files

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-$(document).ready(function() {
-// write your code here
+$(document).ready(function(){
 
+  $.getJSON("data.json", function(data){
+    var spot_data = '';
+    $.each(data, function(key, value){
+      spot_data += '<tr>';
+      spot_data += '<td>'+value.name+'</td>';
+      spot_data += '<td>'+value.description+'</td>';
+      spot_data += '<td>'+ "<a href="+"https://www.google.com/maps?q="+value.location+'>Open in Google Maps</td>';
+      spot_data += '</tr>';
+    })
+    $('tbody').append(spot_data);
+  });
 });
+  
